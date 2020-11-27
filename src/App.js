65008d2dcfe983a547ebb32d2e6866ec02fdc457
Adam Wilson <Adw1995@yahoo.com>
2020-11-27 13:32:28 -0500

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>
+          This is just the beginning! 
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+  
+  const myfirstelement = <h1> Hello React JS! </h1>
+
+  ReactDOM.render(myfirstelement, document.getElementById("root"));
+  
+
+
+
